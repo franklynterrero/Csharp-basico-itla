4a4d1f5b514eb6130371774a07d8c90ccd59139b
franklynterrero <franklin.t.flores@hotmail.com>
2024-08-21 21:08:56 -0400

    there are the homework for while and for ft

--- Csharp Basico and . NET/Csharp Basico and . NET/Program.cs
+++ Csharp Basico and . NET/Csharp Basico and . NET/Program.cs
@@ -1,10 +1,15 @@
-﻿using Csharp_Basico_and_._NET.Calcular_2_numeros;
+﻿using Csharp_Basico_and_._NET.Aritmetica;
+using Csharp_Basico_and_._NET.Calcular_2_numeros;
 using Csharp_Basico_and_._NET.Calificaciones;
+using Csharp_Basico_and_._NET.Conjuntos;
+using Csharp_Basico_and_._NET.Piezas;
+using Csharp_Basico_and_._NET.Suma_Conjunto;
 using Csharp_Basico_and_._NET.Test;
+using Csharp_Basico_and_._NET.Triangulos;
 
-Calificaciones calificaciones = new Calificaciones();
-calificaciones.CalcularCalificaiones();
-Console.ReadLine();
+//Calificaciones calificaciones = new Calificaciones();
+//calificaciones.CalcularCalificaiones();
+//Console.ReadLine();
 
 //Calcular calcular = new Calcular();
 //calcular.CalcularNumeros();
@@ -12,4 +17,24 @@ Console.ReadLine();
 
 //Test test = new Test();
 //test.TestPreguntas();
+//Console.ReadLine();
+
+//Triangulos triangulos = new Triangulos();
+//triangulos.CalcularDimensiones();
+//Console.ReadLine();
+
+//conjuntos conjuntos =  new conjuntos();
+//conjuntos.CalcularConjuntos();
+//Console.ReadLine();
+
+//SumaConjuntos sumaConjuntos = new SumaConjuntos();
+//sumaConjuntos.SumaFinalesConjuntos();
+//Console.ReadLine(); 
+
+Aritmetica aritmetica = new Aritmetica();
+aritmetica.CalcularAritmetica();
+Console.ReadLine();
+
+//piezas piezas = new piezas();
+//piezas.CalcularPiezas();
 //Console.ReadLine();
---
+++ Csharp Basico and . NET/Csharp Basico and . NET/Triangulos/Triangulos.cs
@@ -0,0 +1,43 @@
+﻿namespace Csharp_Basico_and_._NET.Triangulos
+{
+    public class Triangulos
+    {
+        /// <summary>
+        ///  1) Dado un conjunto de datos representando las dimensiones de triángulos, 
+        ///  calcular y contar aquellos cuya área excede un valor límite.
+        /// </summary>
+        public void CalcularDimensiones()
+        {
+            int limite = 0;
+            int triangulos = 0;
+            int cantidad = 0;
+            int superior = 0;
+            int inferior = 0;
+            try
+            {
+                Console.WriteLine("Cual es su limite? ");
+                limite = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Cuantos triangulos desea registrar?");
+                cantidad = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= cantidad; i++)
+                {
+                    Console.WriteLine($"Digite el valor del area del triangulo {i}");
+                    triangulos = Convert.ToInt32(Console.ReadLine());
+                    if (triangulos > limite)
+                    {
+                        superior++;
+                    }
+                    else
+                    {
+                        inferior++;
+                    }
+                }
+                Console.WriteLine($"Cantidad de triangulos superiores al limite: {superior}, Cantidad de triangulos menores al limite {inferior}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error revisar: {ex.Message}");
+            }
+        }
+    }
+}
